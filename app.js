6b33acf4ea93ff8f8c5de90526cc6bd94f55be32
Roberto <Rotamiura@gmail.com>
2026-06-02 17:01:00 -0300

    Add database initialization in App component
    Initialize the database when the App component mounts.

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-import Routes from './src/navigation/routes';
+import React, {
+  useEffect,
+} from 'react';
+
+import Routes
+from './navigation/routes';
+
+import {
+  initDatabase,
+} from './database/database';
 
 export default function App() {
+
+  useEffect(() => {
+
+    initDatabase();
+
+  }, []);
+
   return <Routes />;
 }
